a9aaaa773af45c6867bc28d85f0c7175c396d734
linearboom <127944151+linearboom@users.noreply.github.com>
2023-03-17 15:20:49 +0530

    Add files via upload

---
+++ Patterns Page 2/Pattern7.java
@@ -0,0 +1,30 @@
+class Pattern7{
+	public static void main(String args[]){
+		int counter = 1;
+		for (int i = 1 ; i<= 4; i++){
+			for(int j = 1; j<=5-i; j++){
+				System.out.print(" ");
+			}
+				
+			for(int j = 1; j<=counter;j++){
+				System.out.print("*");
+			}
+			System.out.println("");
+			counter+=2;
+			
+		}
+		counter = 9;
+		for (int i = 1 ; i<= 5; i++){
+			for(int j = 1; j<i; j++){
+				System.out.print(" ");
+			}
+				
+			for(int j = 1; j<=counter;j++){
+				System.out.print("*");
+			}
+			System.out.println("");
+			counter-=2;
+			
+		}
+	}
+}
